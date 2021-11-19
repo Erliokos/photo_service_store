53d517e27dd2252362d95028a6ab53c8ce7c93a9
albertmurzakov <albert.mak.art@gmail.com>
2021-11-19 15:07:09 +0300

    услуги добавляються

--- routes/service.router.js
+++ routes/service.router.js
@@ -1,8 +1,31 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { SERVICE, ZAKAZ, USER, ZAKAZ_SERVICE } = require("../db/models");
 
-router.route('/')
-.get((req, res) => {
-  res.render('service')
-})
+router
+  .route("/")
+  .get(async (req, res) => {
+    const serv = await SERVICE.findAll();
+    res.render("service", { serv });
+  })
+  .post(async (req, res) => {
+    if (res.locals.user && req.body) {
+      const arr = Object.keys(req.body);
+      let total_price = 0;
+      for(let i=0; i<arr.length; i++){
+        console.log(arr[i]);
+        let tt = await SERVICE.findOne({where: {name:arr[i]}})
+        total_price = total_price + tt.price
+      }
+      console.log(total_price);
+      const zakaz = await ZAKAZ.create({
+        user_id: res.locals.user.id,
+        total_price,
+        status: "неоплачен",
+      });
+      res.redirect("/");
+    } else if (res.locals.user) {
+      res.redirect("/signin");
+    }
+  });
 
-module.exports = router
+module.exports = router;
