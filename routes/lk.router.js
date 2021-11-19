fa363b47e90aaaa31a599fe7c8edcb0a91cc135e
Nikita <balykin230195@gmail.com>
2021-11-19 15:14:32 +0300

    ручки 4

--- routes/lk.router.js
+++ routes/lk.router.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
+const {ZAKAZ, ZAKAZ_SERVICE, SERVICE, USER} = require('../db/models')
 
 router.route('/')
-.get((req, res) => {
+.get(async (req, res) => {
+  const aaa = await ZAKAZ_SERVICE.create({zakaz_id:2, service_id:3})
+  // const aaa = await ZAKAZ.findAll({ 
+  //   where: {user_id: req.session.user.id}
+  // })
+  console.log(aaa);
   res.render('lk')
+  
 })
 .post((req, res) => {
   console.log(req.body)
