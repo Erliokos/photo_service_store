39c1eb446167929d526dddfaa63b61dc062b11a8
talgat.aimbetov.b@gmail.com <84536810+Frataim@users.noreply.github.com>
2021-11-19 15:10:24 +0300

    nichego

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const hbs = require('hbs');
-require('dotenv').config();
-const indexRouter = require('./routes/index.router');
-const signupRouter = require('./routes/signup.router');
-const signinRouter = require('./routes/signin.router');
-const serviceRouter = require('./routes/service.router');
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+require('dotenv').config()
+const indexRouter = require('./routes/index.router')
+const signupRouter = require('./routes/signup.router')
+const signinRouter = require('./routes/signin.router')
+const serviceRouter = require('./routes/service.router')
 const portfolioRouter = require('./routes/portfolio.router')
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
 
+const app = express()
 
-const app = express();
+app.set('view engine', 'hbs')
+app.set('views', path.join(process.env.PWD, 'src', 'views'))
+hbs.registerPartials(path.join(process.env.PWD, 'src', 'views', 'partials'))
 
-
-
-
-app.set('view engine', 'hbs');
-app.set('views', path.join(process.env.PWD, 'src', 'views'));
-hbs.registerPartials(path.join(process.env.PWD, 'src', 'views', 'partials'));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(process.env.PWD, 'public')));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(process.env.PWD, 'public')))
 app.use(
-   session( {
+  session({
     store: new FileStore(), // хранилище сессий
     key: 'sid', // ключ куки
     secret: 'gchjtghjkl;bjkll', // шифрование id сессии
@@ -34,20 +29,18 @@ app.use(
     httpOnly: true, // нельзя изменить куки с фронта
     cookie: { expires: 24 * 60 * 60e3 },
   })
-  ) 
-  app.use((req, res, next) => {
-    res.locals.user= req.session.user
-    next()
-  })
+)
+app.use((req, res, next) => {
+  res.locals.user = req.session.user
+  next()
+})
 
-app.use('/', indexRouter);
-app.use('/signup', signupRouter);
-app.use('/signin', signinRouter);
+app.use('/', indexRouter)
+app.use('/signup', signupRouter)
+app.use('/signin', signinRouter)
 app.use('/service', serviceRouter)
-app.use('/porfolio', portfolioRouter);
+app.use('/porfolio', portfolioRouter)
 
+const PORT = process.env.PORT || 3000
 
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => console.log(`Vsyo horosho na porty ${PORT}`));
+app.listen(PORT, () => console.log(`Vsyo horosho na porty ${PORT}`))
--- routes/signup.router.js
+++ routes/signup.router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {USER} = require('../db/models')
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 router.route('/')
 .get((req, res) => {
